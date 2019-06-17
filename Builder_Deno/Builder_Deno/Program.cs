6bd5aff78a14e95f89927e3caca1c04a5c7b0958
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-17 11:18:47 +0200

    Init Builder

---
+++ Builder_Deno/Builder_Deno/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_Deno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
