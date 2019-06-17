8a74049e62c96e627afd5958b7d76f1a922bc7f5
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-17 14:05:27 +0200

    Init Observer

---
+++ Observer_Demo/Observer_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
