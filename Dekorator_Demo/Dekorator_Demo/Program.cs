e44677db8072e4aef6f749c07e9a1647218e38b3
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-17 13:40:15 +0200

    Init Dekorator

---
+++ Dekorator_Demo/Dekorator_Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dekorator_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
