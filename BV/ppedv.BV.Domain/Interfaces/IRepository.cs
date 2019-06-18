b65eb476d907c11ea62d8b2051de5f85fb8d3a82
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 11:10:21 +0200

    Interface für das Repositorypattern erstellt

---
+++ BV/ppedv.BV.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.BV.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        T GetByID<T>(int ID) where T : Entity;
+        IQueryable<T> Query<T>() where T : Entity;
+        void Add<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Update<T>(T item) where T : Entity;
+        void Save();
+    }
+}
