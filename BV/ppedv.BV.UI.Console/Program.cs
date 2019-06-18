96e97472f3d6956607788f044575bc286441e283
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 13:53:41 +0200

    Init Konsole

---
+++ BV/ppedv.BV.UI.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.BV.UI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
