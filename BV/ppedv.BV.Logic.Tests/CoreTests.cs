2dbbe0b05973dc4ba8e1f01f581e13418b3aa502
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 13:34:45 +0200

    XUnit vergleich in den Links

--- BV/ppedv.BV.Logic.Tests/CoreTests.cs
+++ BV/ppedv.BV.Logic.Tests/CoreTests.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Moq;
+using ppedv.BV.Data.EF;
 using ppedv.BV.Domain;
 using ppedv.BV.Domain.Interfaces;
 using System;
@@ -13,6 +15,8 @@ namespace ppedv.BV.Logic.Tests
 
     public class CoreTests
     {
+        public const string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=BV_Tests;Trusted_Connection=true;AttachDbFilename=C:\temp\BV_Test.mdf";
+
         [Fact]
         [Trait("Unittest","")]
         public void Core_CreateNewBookStore_calls_RepositoryAdd_5_times_and_saves_changes()
@@ -26,5 +30,20 @@ namespace ppedv.BV.Logic.Tests
             mock.Verify(x => x.Add(It.IsAny<BookStore>()), Times.Exactly(5));
             mock.Verify(x => x.Save(), Times.AtLeastOnce());
         }
+
+        [Fact]
+        [Trait("Integrationstest","")]
+        public void Core_can_CreateNewBookStore()
+        {
+            var repository = new EFRepository(new EFContext(connectionString));
+            var core = new Core(repository);
+
+            var oldCount = repository.Query<BookStore>().Count(); 
+            core.CreateNewBookStore();
+            var newCount = repository.Query<BookStore>().Count();
+
+            newCount.Should().Be(oldCount + 5);
+        }
+
     }
 }
