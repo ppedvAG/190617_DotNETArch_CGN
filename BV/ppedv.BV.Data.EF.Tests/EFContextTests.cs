6b83088bd63f7f4f153f2d7e95c5dfb048253699
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 09:32:06 +0200

    Erste EFContext-Tests erstellt

---
+++ BV/ppedv.BV.Data.EF.Tests/EFContextTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ppedv.BV.Data.EF.Tests
+{
+    [TestFixture]
+    public class EFContextTests
+    {
+        private const string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=BV_Tests;Trusted_Connection=true";
+        // AAA - Prinzip
+        // Arrange, Act, Assert
+
+        // Namensgebung von Testmethoden:
+        // NameDerKlasse_NameDerMethode_Szenario_ErwartetesErgebnis
+        // Beispiel: Taschenrechner_Sum_2_and_2_results_4
+
+        [Test]
+        public void EFContext_can_create_context()
+        {
+            var context = new EFContext(connectionString);
+        }
+
+        [Test]
+        public void EFContext_can_create_database()
+        {
+            using (var context = new EFContext(connectionString))
+            {
+                if (context.Database.Exists())
+                    context.Database.Delete();
+
+                Assert.IsFalse(context.Database.Exists());
+
+                context.Database.Create();
+
+                Assert.IsTrue(context.Database.Exists());
+            }
+        }
+
+
+    }
+}
