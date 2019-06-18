64098182da1a58f982677cc610cae210fad16463
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 11:22:13 +0200

    EFRepository erstellt

---
+++ BV/ppedv.BV.Data.EF/EFRepository.cs
@@ -0,0 +1,55 @@
+﻿using ppedv.BV.Domain;
+using ppedv.BV.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.BV.Data.EF
+{
+    public class EFRepository : IRepository
+    {
+        public EFRepository(EFContext context)
+        {
+            this.context = context;
+        }
+        private readonly EFContext context;
+
+        public void Add<T>(T item) where T : Entity
+        {
+            context.Set<T>().Add(item);
+        }
+
+        public void Delete<T>(T item) where T : Entity
+        {
+            context.Set<T>().Remove(item);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToArray();
+        }
+
+        public T GetByID<T>(int ID) where T : Entity
+        {
+            return context.Set<T>().Find(ID);
+        }
+
+        public IQueryable<T> Query<T>() where T : Entity
+        {
+            return context.Set<T>();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T item) where T : Entity
+        {
+            var loadedItem = GetByID<T>(item.ID);
+            if (loadedItem != null)
+                context.Entry(loadedItem).CurrentValues.SetValues(item);
+        }
+    }
+}
