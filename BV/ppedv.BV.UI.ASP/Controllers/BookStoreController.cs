43fe34d26f0e0a1ea918634a602954dc2d356928
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 14:28:12 +0200

    Logik im Controller ausprogrammiert

--- BV/ppedv.BV.UI.ASP/Controllers/BookStoreController.cs
+++ BV/ppedv.BV.UI.ASP/Controllers/BookStoreController.cs
@@ -1,4 +1,6 @@
 ﻿using ppedv.BV.Data.EF;
+using ppedv.BV.Domain;
+using ppedv.BV.Domain.Interfaces;
 using ppedv.BV.Logic;
 using System;
 using System.Collections.Generic;
@@ -13,35 +15,38 @@ namespace ppedv.BV.UI.ASP.Controllers
         private const string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=BV_Produktiv;Trusted_Connection=true;AttachDbFilename=C:\temp\BV.mdf";
         public BookStoreController()
         {
-            core = new Core(new EFRepository(new EFContext(connectionString)));
+            repository = new EFRepository(new EFContext(connectionString));
+            core = new Core(repository);
         }
         private readonly Core core;
+        private readonly IRepository repository;
 
         // GET: BookStore
         public ActionResult Index()
         {
-            return View();
+            return View(repository.GetAll<BookStore>());
         }
 
         // GET: BookStore/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            return View(repository.GetByID<BookStore>(id));
         }
 
         // GET: BookStore/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new BookStore { Address = "Neuegasse 22" });
         }
 
         // POST: BookStore/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(BookStore newBookStore) //(FormCollection collection)
         {
             try
             {
-                // TODO: Add insert logic here
+                repository.Add(newBookStore);
+                repository.Save();
 
                 return RedirectToAction("Index");
             }
@@ -54,16 +59,17 @@ namespace ppedv.BV.UI.ASP.Controllers
         // GET: BookStore/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            return View(repository.GetByID<BookStore>(id));
         }
 
         // POST: BookStore/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, BookStore editedBookStore)
         {
             try
             {
-                // TODO: Add update logic here
+                repository.Update(editedBookStore);
+                repository.Save();
 
                 return RedirectToAction("Index");
             }
@@ -76,22 +82,24 @@ namespace ppedv.BV.UI.ASP.Controllers
         // GET: BookStore/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            return View(repository.GetByID<BookStore>(id));
         }
 
         // POST: BookStore/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, BookStore _NOTused_)
         {
             try
             {
-                // TODO: Add delete logic here
+                var bookToDelete = repository.GetByID<BookStore>(id);
+                repository.Delete(bookToDelete);
+                repository.Save();
 
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return RedirectToAction("Index");
             }
         }
     }
