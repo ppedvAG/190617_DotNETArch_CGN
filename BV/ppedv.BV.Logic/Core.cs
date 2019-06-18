93c6dd280523e8adcc48f7a3a60436d494402737
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 11:36:52 +0200

    Core erstellt und eine Methode als "Geschäftslogik" eingefügt

--- BV/ppedv.BV.Logic/Core.cs
+++ BV/ppedv.BV.Logic/Core.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using AutoFixture;
+using ppedv.BV.Domain;
+using ppedv.BV.Domain.Interfaces;
+using System;
 
 namespace ppedv.BV.Logic
 {
     public class Core
     {
+        public Core(IRepository repository)
+        {
+            this.fixture = new Fixture();
+            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        private readonly IRepository repository;
+        private readonly Fixture fixture;
+
+        // Geschäftslogik:
+        public void CreateNewBookStore()
+        {
+            var newBookStores = fixture.CreateMany<BookStore>(5);
+            foreach (var store in newBookStores)
+            {
+                repository.Add(store);
+            }
+            repository.Save();
+        }
     }
 }
