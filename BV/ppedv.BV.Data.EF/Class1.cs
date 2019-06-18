43d6816ad4de905068ac9c523b7d9c3e40a9d3a0
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-18 09:22:34 +0200

    Simpler EFContext erstellt

--- BV/ppedv.BV.Data.EF/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ppedv.BV.Data.EF
-{
-    public class Class1
-    {
-    }
-}
---
+++ BV/ppedv.BV.Data.EF/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using ppedv.BV.Domain;
+using System;
+using System.Data.Entity;
+
+namespace ppedv.BV.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : this(@"Server=(localdb)\MSSQLLocalDB;Database=BV;Trusted_Connection=true") {}
+        public EFContext(string connectionString) : base(connectionString){}
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<BookStore> BookStore { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+    }
+}
