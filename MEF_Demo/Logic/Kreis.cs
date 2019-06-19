baef14bd1c2446d4830932ffe30c7256431df4de
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-19 11:19:22 +0200

    Logik (ohne MEF-Code) erstellt

---
+++ MEF_Demo/Logic/Kreis.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Kreis : IGrafik
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Zeichnen()
+        {
+            Console.WriteLine($"Ein Kreis mit den Koordinaten {X}/{Y} wird gezeichnet");
+        }
+    }
+}
---
+++ MEF_Demo/Logic/Rechteck.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using System;
+
+namespace Logic
+{
+    public class Rechteck : IGrafik
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Zeichnen()
+        {
+            Console.WriteLine($"Ein Rechteck mit den Koordinaten {X}/{Y} wird gezeichnet");
+        }
+    }
+}
