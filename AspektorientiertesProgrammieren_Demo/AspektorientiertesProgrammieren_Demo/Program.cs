4a2649a0f0b664bf09cd6155337339f72a4a31ed
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-19 11:40:59 +0200

    Init Aspekte

---
+++ AspektorientiertesProgrammieren_Demo/AspektorientiertesProgrammieren_Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspektorientiertesProgrammieren_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
