f1898af91a6be02cfe04db839d047d0af63a8e92
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-19 11:30:27 +0200

    Einstieg MEF

--- MEF_Demo/Logic/Kreis.cs
+++ MEF_Demo/Logic/Kreis.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel.Composition; // MEF
 namespace Logic
 {
+    [Export(typeof(IGrafik))] // Klasse Exportieren
     public class Kreis : IGrafik
     {
         public int X { get; set; }
         public int Y { get; set; }
 
+        [Export("Zeichnen_Methode")] // Logik unter dem Namen XYZ exportieren
         public void Zeichnen()
         {
             Console.WriteLine($"Ein Kreis mit den Koordinaten {X}/{Y} wird gezeichnet");
--- MEF_Demo/MEF_Demo/Program.cs
+++ MEF_Demo/MEF_Demo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Domain;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,18 @@ namespace MEF_Demo
     {
         static void Main(string[] args)
         {
+            // Vorbereitung für MEF
+            var catalog = new DirectoryCatalog("."); // Gibt die Quelle der Libs an
+            var container = new CompositionContainer(catalog);
 
+            MSPaint paint = new MSPaint();
+            container.ComposeParts(paint); // Ladet alles in pain hinein
+
+            // Variante 1: Klasse
+            //paint.Zeichnung.Zeichnen();
+
+            // Variante 2: Methode
+            paint.LogikVonWoanders.Invoke();
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
@@ -20,6 +33,10 @@ namespace MEF_Demo
 
     class MSPaint
     {
+        [Import("Zeichnen_Methode")]
+        public Action LogikVonWoanders;
+
+        [Import(typeof(IGrafik))]
         public IGrafik Zeichnung { get; set; }
     }
 }
